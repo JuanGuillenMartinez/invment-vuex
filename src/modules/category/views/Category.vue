ad4c766ee10c73930e63d0927724e0f9b4e75f2b
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-04 15:19:47 -0600

    Terminada vista de categoria por ID

--- src/modules/category/views/Category.vue
+++ src/modules/category/views/Category.vue
@@ -1,13 +1,71 @@
 <template>
-  <h1>Este es el componente para mostrar categorías</h1>
+    <div class="container-info-category">
+        <div class="card card-body">
+            <div class="info-category">
+                <h2 class="card-title">Nombre de la categoría</h2>
+                <div>
+                    <label class="form-label">ID de categoría</label>
+                    <input
+                        value="1"
+                        type="number"
+                        class="form-control"
+                        readonly
+                    />
+                </div>
+                <div>
+                    <label class="form-label">Nombre</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Nombre de la categoría"
+                    />
+                </div>
+                <div>
+                    <label class="form-label">Creado el</label>
+                    <input
+                        type="datetime-local"
+                        class="form-control"
+                        id="created-at"
+                        name="created_at"
+                        value="2022-02-04T19:30"
+                    />
+                </div>
+                <div>
+                    <label class="form-label">Creado el</label>
+                    <input
+                        type="datetime-local"
+                        class="form-control"
+                        id="updated_at"
+                        name="updated_at"
+                        value="2022-02-04T19:30"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+    <float-button />
 </template>
 
 <script>
+import FloatButton from "../components/FloatButton.vue";
+
 export default {
-
-}
+    components: {
+        FloatButton,
+    },
+};
 </script>
-
-<style>
-
+<style scoped>
+.info-category {
+    margin: 24px;
+}
+.container-info-category {
+    margin: 16px;
+}
+.card-title {
+    margin-bottom: 32px;
+}
+input {
+    margin-bottom: 12px;
+}
 </style>
