4d5a62a53aef1874f64d5065c01329c09b25af96
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-04 13:42:34 -0600

    Vista de categoria no seleccionada terminada

---
+++ src/modules/category/views/NoEntrySelected.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Categoría no seleccionada</h1>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+    align-self: center;
+}
+h1 {
+    text-align: center;
+}
+</style>
