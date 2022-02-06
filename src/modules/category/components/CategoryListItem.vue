9160e45b1fe181f13db407659d0500856795a341
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-06 16:40:11 -0600

    Cambios en los estilos del index de categories

--- src/modules/category/components/CategoryListItem.vue
+++ src/modules/category/components/CategoryListItem.vue
@@ -1,17 +1,20 @@
 <template>
     <div @click="goToCategory()" class="card card-body">
-        <h4 class="card-title">Titulo de la categoría</h4>
-        <p class="card-text">
+        <span>{{ category.name }}</span>
+        <!-- <p class="card-text">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
             quidem sed? Reprehenderit iste quasi dolor ratione magnam aperiam
             assumenda odio voluptatum recusandae autem nobis repudiandae
             excepturi numquam, perspiciatis consectetur aliquam.
-        </p>
+        </p> -->
     </div>
 </template>
 
 <script>
 export default {
+    props: {
+        category: Object
+    },
     methods: {
         goToCategory() {
             this.$router.push( { name: 'category', params: { id: 10 } } )
@@ -27,5 +30,11 @@ export default {
 div {
     cursor: pointer;
     margin-bottom: 10px;
+}
+.card-body {
+    border-radius: 12px;
+}
+span {
+    font-size: 16px;
 }
 </style>
