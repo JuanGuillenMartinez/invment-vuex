fcb2974e6c6546c988682a9f492813ce4c9c14e0
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-06 18:51:57 -0600

    Añadida validacion de enteros mediante guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,41 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import auth from '../modules/auth/router'
-import categoriesRoutes from '../modules/category/router'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import auth from "../modules/auth/router";
+import categoriesRoutes from "../modules/category/router";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/auth',
-    ...auth
-  },
-  {
-    path: '/categories',
-    ...categoriesRoutes
-  },
-]
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+        path: "/auth",
+        ...auth,
+    },
+    {
+        path: "/categories",
+        ...categoriesRoutes,
+    },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
-
-export default router
+    history: createWebHashHistory(),
+    routes,
+});
+router.beforeEach((to) => {
+    if (to.name === "category") {
+        if (isNaN(parseInt(to.params.id))) return "/categories";
+        else return true;
+    }
+});
+export default router;
