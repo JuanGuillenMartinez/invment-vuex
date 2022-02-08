85f343d1b90656fe779b5076dec1d79dc91c7cfb
Juan Guillen Martinez <juanguillenmtz16@gmail.com>
2022-02-07 20:26:59 -0600

    removido las librerias de datetime

--- src/main.js
+++ src/main.js
@@ -7,4 +7,7 @@ import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
+import flatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
+
 createApp(App).use(store).use(router).mount("#app");
--- src/modules/category/views/CategoryItem.vue
+++ src/modules/category/views/CategoryItem.vue
@@ -21,26 +21,6 @@
                         placeholder="Nombre de la categoría"
                     />
                 </div>
-                <div>
-                    <label class="form-label">Creado el</label>
-                    <input
-                        :value="currentCategory.created_at"
-                        type="datetime-local"
-                        class="form-control"
-                        id="created_at"
-                        name="created_at"
-                    />
-                </div>
-                <div>
-                    <label class="form-label">Actualizado el</label>
-                    <input
-                        :value="currentCategory.updated_at"
-                        type="datetime-local"
-                        class="form-control"
-                        id="updated_at"
-                        name="updated_at"
-                    />
-                </div>
             </div>
         </div>
     </div>
@@ -50,14 +30,14 @@
 <script>
 import FloatButton from "../components/FloatButton.vue";
 import { mapGetters } from "vuex";
+
 export default {
     components: {
         FloatButton,
     },
     data() {
         return {
-            currentCategory: null,
-            currentDate: null,
+            currentCategory: null,            
         };
     },
     props: {
@@ -68,12 +48,6 @@ export default {
     },
     computed: {
         ...mapGetters("category", ["categoryById"]),
-        created_at() {
-            const utcDate = new Date(this.currentCategory.created_at);
-            // const localDate = utcDate.toLocaleString();
-            // console.log(localDate);
-            return utcDate;
-        },
         // updated_at() {
 
         // }
